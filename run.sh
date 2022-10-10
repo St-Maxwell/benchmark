4f08e67298a8fea8c211817e91165748bfe6a774
Nugine <nugine@foxmail.com>
2022-10-10 22:40:52 +0800

    add clang benchmark

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 # compile
 gfortran test_cases.f90 -Ofast -march=native -o fortran_benchmark
-g++ test_cases.cpp -Ofast -march=native -o cpp_benchmark
+g++ test_cases.cpp -Ofast -march=native -o cpp_gcc_benchmark
+clang++ test_cases.cpp -Ofast -march=native -o cpp_clang_benchmark
 rustc test_cases.rs -C opt-level=3 -C target-cpu=native -o rust_benchmark
 
 # run
 ./fortran_benchmark | tee fortran_results.txt
-./cpp_benchmark | tee cpp_results.txt
+./cpp_gcc_benchmark | tee 'cpp(gcc)_results.txt'
+./cpp_clang_benchmark | tee 'cpp(clang)_results.txt'
 ./rust_benchmark | tee rust_results.txt
 
 OS=$(lsb_release -d | cut -f2 -d:)
@@ -17,7 +19,7 @@ CPU=$(cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq)
 python3 plot_to_html.py ${GITHUB_SHA::8} "$OS" "$CPU"
 
 # deploy
-if [ ! -d "/page" ]; then
+if [ ! -d "./page" ]; then
   mkdir ./page
 fi
 mv index.html ./page/index.html
